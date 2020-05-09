c121b794d1eba9b39842195716273bb676f0656e
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2020-05-10 01:28:08 +0800

    Update SimpleWebClockWithTimer.py

--- SimpleWebClockWithTimer.py
+++ SimpleWebClockWithTimer.py
@@ -1,7 +1,7 @@
 # Timer-based simple clock with SSD1306 oled display (by Alan Wang)
 
-SSID = "your_wifi_ssid"     # WiFi ssid
-PW   = "your_wifi_password" # WiFi password
+SSID = 'your_wifi_ssid'     # WiFi ssid
+PW   = 'your_wifi_password' # WiFi password
 
 TMZ_HOUR_OFFSET = 0 # timezone hour offset
 
@@ -9,7 +9,7 @@ import network, urequests, utime, ntptime, ssd1306
 from machine import Pin, I2C, Timer
 
 # dictionary for weekday names
-weekday = {0:"Mon", 1:"Tue", 2:"Wed", 3:"Thr", 4:"Fri", 5:"Sat", 6:"Sun"}
+weekday = {0:'Mon', 1:'Tue', 2:'Wed', 3:'Thr', 4:'Fri', 5:'Sat', 6:'Sun'}
 
 # setup oled display (SCL -> D1, SDA -> D2)
 display = ssd1306.SSD1306_I2C(128, 64, I2C(scl=Pin(5), sda=Pin(4)))
@@ -34,9 +34,9 @@ def ntpUpdate(timer):
 def clockUpdate(timer):
     localTime = utime.localtime(utime.time() + TMZ_HOUR_OFFSET * 3600)
     display.fill(0)
-    display.text("{0:04d}-{1:02d}-{2:02d} ".format(*localTime) +
+    display.text('{0:04d}-{1:02d}-{2:02d} '.format(*localTime) +
                  weekday[localTime[6]], 8, 16)
-    display.text("{3:02d}:{4:02d}:{5:02d} ".format(*localTime), 32, 40)
+    display.text('{3:02d}:{4:02d}:{5:02d} '.format(*localTime), 32, 40)
     display.show() # display clock
 
 # update time for the first time
