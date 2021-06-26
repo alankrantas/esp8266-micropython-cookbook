8951627cb67f59bf3b2b5b1679aeb7a301776002
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2021-06-27 03:27:01 +0800

    Update WebJSONQuery_Template.py

--- WebJSONQuery_Template.py
+++ WebJSONQuery_Template.py
@@ -1,35 +1,43 @@
 # ESP8266 Web JSON Query Template
 
-import network, urequests, utime
-from machine import reset
+import network, urequests, utime, sys
 
 # user info
-wifi_ssid = '' # change this to your WiFi AP name
-wifi_pw   = '' # change this to your WiFi AP password
+ssid = '' # change this to your WiFi AP name
+pw   = '' # change this to your WiFi AP password
 
 # API url
-api_url   = 'http://api.open-notify.org/iss-now.json'
+api_url = 'http://api.open-notify.org/iss-now.json'
+
+
+# wifi error descriptions
+wifi_error = {
+    network.STAT_WRONG_PASSWORD: 'wrong password',
+    network.STAT_NO_AP_FOUND: 'wifi AP not found',
+    network.STAT_CONNECT_FAIL: 'due to other problems',
+    }
 
 # connecting to WiFi
 wifi = network.WLAN(network.STA_IF)
 wifi.active(True)
-wifi.connect(wifi_ssid, wifi_pw)
+wifi.connect(ssid, pw)
 
-print('ESP8266 connecting to WiFi:', wifi_ssid)
-while not wifi.isconnected():
+print('ESP8266 connecting to', ssid, '...')
+while wifi.status() == network.STAT_CONNECTING:
     pass
+        
+if wifi.status() != network.STAT_GOT_IP:
+    print('Failed to connect:', wifi_error[wifi.status()])
+    sys.exit()
+
 print('Connected.')
 print('IP:', wifi.ifconfig()[0], '\n')
+utime.sleep(1)
+
 
 while True:
     
-    # reboot device if WiFi connectio is lost
-    if not wifi.isconnected():
-        print('WiFi connection lost. Rebooting...')
-        reset()
-    
     try:
-        
         # send HTTP GET request to the API
         print('Querying API:', api_url)
         response = urequests.get(api_url)
@@ -53,4 +61,4 @@ while True:
               'the API is not supported by MicroPython.')
 
     print('')
-    utime.sleep(15)
+    utime.sleep(30)
