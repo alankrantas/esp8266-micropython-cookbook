2e20e890499743edf15399ea61aa83ef937274c9
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2022-02-08 13:40:47 +0800

    Update Simple_WebServer.py

--- Simple_WebServer.py
+++ Simple_WebServer.py
@@ -11,31 +11,53 @@ led = Pin(2, Pin.OUT, value=1)
 
 
 # webpage template
-html  = r'<!DOCTYPE html>'
-html += r'<html>'
-html += r'<head>'
-html += r'<title>ESP8266 Web Server</title>'
-html += r'<meta name="viewport" content="width=device-width, initial-scale=1">'
-html += r'<link rel="icon" href="data:,">'
-html += r'<style>body {background-color: Moccasin;} h1 {color: SaddleBrown;} h2 {color: Olive;} </style>'
-html += r'</head>'
-html += r'<body><center>'
-html += r'<h1>ESP8266 Web Server</h1>'
-html += r'<h2>LED status: {led_status}</h2>'
-html += r'<form methon="GET" action="">'
-html += r'<p><input id="led_on" type="submit" name="led" value="On"></p>'
-html += r'<p><input id="led_off" type="submit" name="led" value="Off"></p>'
-html += r'</form></center></body>'
-html += r'</html>'
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ESP8266 Web Server</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="data:," />
+        <style>
+            body {
+            background-color: Moccasin;
+            }
+            h1 {
+            color: SaddleBrown;
+            }
+            h2 {
+            color: Olive;
+            }
+        </style>
+    </head>
+    <body>
+        <center>
+            <h1>ESP8266 Web Server</h1>
+            <h2>LED status: <!--led_status--></h2>
+            <form methon="GET" action="">
+                <p><input id="led_on" type="submit" name="led" value="On" /></p>
+                <p><input id="led_off" type="submit" name="led" value="Off" /></p>
+            </form>
+        </center>
+    </body>
+</html>
+"""
+
+# add HTTP response headers
+http_resp = 'HTTP/1.1 200 OK\r\n' + \
+    'Content-Type: text/html\r\n' + \
+    'Connection: close\r\n\r\n'
+
+# parse html to a single string
+html = http_resp + ''.join([line.strip() for line in html.split('\n')])
 
 
 # generated webpage to be sent to user
 def web_page():
     led_status = 'ON' if led.value() == 0 else 'OFF'
-    return html.replace('{led_status}', led_status)
+    return html.replace('<!--led_status-->', led_status)
 
-
-# extract any number of parameter names and values from HTTP response
+# extract url parameters from HTTP request
 def get_paras(get_str):
     para_dict = {}
     q_pos = get_str.find('/?')
@@ -47,13 +69,13 @@ def get_paras(get_str):
             para_dict.update({para_tmp[0] : para_tmp[1]})
     return para_dict
 
+
 # wifi error descriptions
 wifi_error = {
     network.STAT_WRONG_PASSWORD: 'wrong password',
     network.STAT_NO_AP_FOUND: 'wifi AP not found',
     -1: 'due to other problems',
     }
-
 
 # connect to wifi
 print('Connecting to WiFi...')
@@ -66,7 +88,6 @@ while wifi.status() == network.STAT_CONNECTING:
 if wifi.status() != network.STAT_GOT_IP:
     print('Failed to connect:', wifi_error.get(wifi.status(), wifi_error[-1]))
     sys.exit()
-
 
 print('Connected.')
 s = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
